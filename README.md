91169bc78ccef1d02bc170505524559839bf24d0
Rees <40927062+rees047@users.noreply.github.com>
2021-07-16 14:47:14 -0400

    Initial Parcel-Bundler
    - initial commit - testing using Parcel-Bundler
    - package json contains react, reactdom and parcel-bundler
    - index.html, js and scss contains testing html to make sure parcel is working

---
+++ README.md
@@ -0,0 +1,11 @@
+This project is made as the face or client view of the movie-api app
+This will be made using react librarry
+
+dependencies:
+react
+react-dom
+
+node version 14
+npm version 7.20.0
+
+parcel-bundler
---
+++ src/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//import statement to indicate that you need to bundles `./index.scss`
+import  './index.scss';
+
+//main component (will eventually use all the others)
+class CineFilesApp extends React.Component{
+    render(){
+        return (
+            <div className="CineFiles">
+                <div>Good Morning!</div>
+            </div>
+        );
+    }
+}
+
+//Find the root of your app
+const container = document.getElementsByClassName('app-container')[0];
+
+//tells react to render your app in the root DOM element
+ReactDOM.render(React.createElement(CineFilesApp), container);
