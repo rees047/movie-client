160b8dea098d25e2b94db3b618e6d999e881cf73
Rees <40927062+rees047@users.noreply.github.com>
2021-07-30 02:24:11 -0400

    React Redux Initial

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Container from 'react-bootstrap/Container';
-import {MainView} from './components/main-view/main-view';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { devToolsEnhancer } from 'redux-devtools-extension'; //for debugging purposes
+import moviesApp from './reducers/reducers';
 
+import { MainView } from './components/main-view/main-view';
 //import statement to indicate that you need to bundles `./index.scss`
 import  './index.scss';
+
+//const store = createStore(moviesApp);
+const store = createStore(moviesApp, devToolsEnhancer()); //for debugging purposes
 
 //main component (will eventually use all the others)
 class CineFilesApp extends React.Component{
     render(){
         return (
-            <Container id="CineFiles-Client">
-                <MainView />      
-            </Container>
+            <Provider store={store}>
+                <Container id="CineFiles-Client">
+                    <MainView />      
+                </Container>
+            </Provider>
         );
     }
 }
