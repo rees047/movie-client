9496ac76aa5431bb378c37d5bd3425ae5754fd9b
Rees <40927062+rees047@users.noreply.github.com>
2021-07-30 17:02:29 -0400

    converting code to redux

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,11 +1,27 @@
 export const SET_MOVIES = 'SET_MOVIES';
 export const SET_FILTER = 'SET_FILTER';
 
+export const SET_USER = 'SET_USER';
+export const ADD_FAVORITES = 'ADD_FAVORITES';
+export const REMOVE_FAVORITES = 'REMOVE_FAVORITES';
+
 export function setMovies(value){
     return { type: SET_MOVIES, value };
 }
 
 export function setFilter(value){
     return { type: SET_FILTER, value };
+}
+
+export function setUser(value){
+    return { type: SET_USER, value }
+}
+
+export function addFavorites(value){
+    return { type: ADD_FAVORITES, value }
+}
+
+export function removeFavorites(value){
+    return { type: REMOVE_FAVORITES, value }
 }
 
--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 
-import { SET_FILTER, SET_MOVIES } from '../actions/actions';
+import { SET_FILTER, SET_MOVIES, SET_USER, ADD_FAVORITES, REMOVE_FAVORITES } from '../actions/actions';
 
 function visibilityFilter(state = '', action){
     switch (action.type){
@@ -25,6 +25,18 @@ function movies(state = [], action){
 
     }
 }
+
+function user(state = '', action){
+
+    switch (action.type){
+        case SET_USER:
+            return action.value;
+        default:
+            return state;
+    }
+}
+
+
 
 // if combined reducer is not used. take note, return is json obj!
 /*function moviesApp(state = {}, action){
